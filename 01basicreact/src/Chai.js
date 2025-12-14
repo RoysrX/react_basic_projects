2e13762d1962274fe564de57dbc98ac590051451
RoysrX <shirsenduroysr@gmail.com>
2025-12-14 10:33:53 +0530

    commit

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1> chai aur react | roysr</h1>
+    )
+}
+export default Chai;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+    <h1>chai aur react</h1>
+   <Chai />
+   <p>test para</p>
+   </>
+  )
+}
+
+export default App
