f787b586ee9be0f5910358eebcf40b8ae8bc405e
RoysrX <shirsenduroysr@gmail.com>
2025-12-14 12:06:22 +0530

    commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,11 @@ import Chai from "./chai"
 
 function App() {
 
+  const username = "Roysr"
+
   return (
     <>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
    <Chai />
    <p>test para</p>
    </>
